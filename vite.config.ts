8430f555a242126b26c85c76c74f0fc0a8dbd4a9
Owen Harris <owenh0410@gmail.com>
2026-04-14 22:52:27 -0400

    did gh-pages work

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/project3-frontend/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
